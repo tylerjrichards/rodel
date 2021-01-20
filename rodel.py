cd656e005016abd231dbac06c1e57174b1714d8a
Tyler Richards <tylerjrichards@gmail.com>
2021-01-20 11:36:32 -0800

    Add files via upload

--- rodel.py
+++ rodel.py
@@ -1,2 +1,15 @@
 import streamlit as st 
-st.image('rodel.png')
+from PIL import Image
+
+ 
+
+# Create an Image object from an Image
+
+colorImage  = Image.open("rodel.png")
+imageLocation = st.empty()
+ 
+
+# Rotate it by 45 degrees
+for i in range(0,1000, 5):
+	rotated = colorImage.rotate(i)
+	imageLocation.image(rotated)
