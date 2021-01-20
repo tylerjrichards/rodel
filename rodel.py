75802e2588e8f2ff4e4b7426e0ec04cc3dba2fa9
Tyler Richards <tylerjrichards@gmail.com>
2021-01-20 11:46:02 -0800

    Add files via upload

--- rodel.py
+++ rodel.py
@@ -4,12 +4,16 @@ from PIL import Image
  
 
 # Create an Image object from an Image
-st.button('Click to make me keep spinning!')
 colorImage  = Image.open("rodel.png")
 imageLocation = st.empty()
+imageLocation.image(colorImage)
+
+
+if st.button('Click to make me keep spinning!'):
+	for i in range(0,1440, 5):
+		rotated = colorImage.rotate(i)
+		imageLocation.image(rotated)
  
 
 # Rotate it by 45 degrees
-for i in range(0,1000, 5):
-	rotated = colorImage.rotate(i)
-	imageLocation.image(rotated)
+
